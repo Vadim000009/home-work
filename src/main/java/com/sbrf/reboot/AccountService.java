d34d29666cbbed1d03ba0f3e95a3f9083cbb27b2
Vadim Rodionov <tuukvadim@live.com>
2021-12-29 13:18:08 +0300

    Выполнена работа в соответствии с заданием 3. Все тесты пройдены! Фикс в виде отдельной ветки, так как иначе совмещаются две дз :с

---
+++ src/main/java/com/sbrf/reboot/AccountService.java
@@ -0,0 +1,25 @@
+package com.sbrf.reboot;
+
+import com.sbrf.reboot.repository.AccountRepository;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Set;
+
+@RequiredArgsConstructor
+public class AccountService {
+
+    @NonNull
+    private AccountRepository accountRepository;
+
+    public boolean isClientHasContract(long clientId, long contractNumber) {
+        return accountRepository.getAllAccountsByClientId(clientId).contains(contractNumber);
+    }
+
+    public long getClientIdByContractNumber(long contractNumber) {
+        Set<Long> accounts = accountRepository.getAllAccounts();
+        return accounts.stream().filter(
+                (x) -> accountRepository.getAllAccountsByClientId(x).contains(contractNumber)
+        ).findFirst().get();
+    }
+}
