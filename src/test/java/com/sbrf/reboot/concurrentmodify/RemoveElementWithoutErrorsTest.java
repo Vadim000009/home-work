6da43b416e2e64538fcf98fe892bb0c4a98d18bf
Vadim Rodionov <tuukvadim@live.com>
2022-02-02 20:22:09 +0300

    Выполнение дз 8

--- src/test/java/com/sbrf/reboot/concurrentmodify/RemoveElementWithoutErrorsTest.java
+++ src/test/java/com/sbrf/reboot/concurrentmodify/RemoveElementWithoutErrorsTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.ConcurrentModificationException;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RemoveElementWithoutErrorsTest {
@@ -29,9 +31,40 @@ public class RemoveElementWithoutErrorsTest {
 
     }
 
+    /*  Удаление через removeIf */
     @Test
-    public void successRemoveElementWithoutErrors() {
+    public void successRemoveElementWithRemoveIf() {
+        List<Integer> fstList = new ArrayList() {{add(1); add(2); add(3);}};
+        List<Integer> secList = new ArrayList() {{add(2); add(3);}};
+        fstList.removeIf(i -> i == 1);
 
+        assertEquals(fstList, secList);
+    }
+
+    /*  Удаление из ArrayList*/
+    @Test
+    public void successRemoveElementFromArrayList() {
+        List<Integer> fstList = new ArrayList() {{add(1); add(2); add(3);}};
+        List<Integer> secList = new ArrayList() {{add(2); add(3);}};
+        List<Integer> listForDelete = new ArrayList<>();
+
+        fstList.forEach((x) -> {
+            if (x == 1) listForDelete.add(x);
+        });
+        fstList.removeAll(listForDelete);
+
+        assertEquals(fstList, secList);
+    }
+
+    /*  Удаление через stream   */
+    @Test
+    public void successRemoveElementFromStream() {
+        List<Integer> fstList = new ArrayList() {{add(1); add(2); add(3);}};
+        List<Integer> secList = new ArrayList() {{add(2); add(3);}};
+
+        fstList = fstList.stream().filter((x)-> x != 1).collect(Collectors.toList());
+
+        assertEquals(fstList, secList);
     }
 
 }
